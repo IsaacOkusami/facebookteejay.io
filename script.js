b72da76342dc24809dfa1e0f0f1e6bc60c8b8d2e
IsaacOkusami <166865668+IsaacOkusami@users.noreply.github.com>
2024-07-18 19:49:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//console.log('Hello World');
+
+const btn = document.querySelectorAll(".btn");
+console.log(btn);
+
+btn.forEach(function (button) {
+    button.addEventListener("click", function (){
+        //console.log(this);
+        this.classList.toggle('active');
+
+        //to close other buttons
+        if(button.classList.contains("active")) {
+            btn.forEach(function (button){
+                button.classList.remove('active')
+            }); //else 
+            button.classList.toggle('active')
+}
+
+    });
+});
